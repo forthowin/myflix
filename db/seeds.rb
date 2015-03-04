fcfb1a50c358ff065bb70be65a58786d4c26f987
Tho Nguyen <thogotmail@gmail.com>
2015-03-04 00:36:13 -0600

    Create test for models

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: 'Monk', description: 'Great show.', small_cover_url: '/public/tmp/monk.jpg', large_cover_url: '/monk_large.jpg')
+cat_com = Category.create(name: 'TV Comedies')
+cat_dram = Category.create(name: 'TV Drama')
+cat_real = Category.create(name: 'Reality TV')
+
+Video.create(title: 'Monk', description: 'Great show.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_com.id)
+Video.create(title: 'Family Guy', description: 'Great show.', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_com.id)
+Video.create(title: 'South Park', description: 'Funny show.', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_com.id)
+
+Video.create(title: 'Monk', description: 'Great show.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_dram.id)
+Video.create(title: 'Family Guy', description: 'Great show.', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_dram.id)
+Video.create(title: 'South Park', description: 'Funny show.', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_dram.id)
+
+Video.create(title: 'Monk', description: 'Great show.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_real.id)
+Video.create(title: 'Family Guy', description: 'Great show.', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_real.id)
+Video.create(title: 'South Park', description: 'Funny show.', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/monk_large.jpg', category_id: cat_real.id)
---
+++ spec/models/video_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Video do
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+
+  describe "search_by_title" do
+    it "returns an empty array if there is no match" do
+      family_guy = Video.create(title: 'Family Guy', description: 'Funny show')
+      south_park = Video.create(title: 'South Park', description: 'Funny show')
+      expect(Video.search_by_title("familly")).to eq([])
+    end
+
+    it "returns an array of one video if there is an exact match" do
+      family_guy = Video.create(title: 'Family Guy', description: 'Funny show')
+      south_park = Video.create(title: 'South Park', description: 'Funny show')
+      expect(Video.search_by_title("Family Guy")).to eq([family_guy])
+    end
+
+    it "returns an array of one video if there is a similar match" do
+      family_guy = Video.create(title: 'Family Guy', description: 'Funny show')
+      south_park = Video.create(title: 'South Park', description: 'Funny show')
+      expect(Video.search_by_title("Fam")).to eq([family_guy])
+    end
+
+    it "returns an array of all matches in order of created_at" do
+      family_guy = Video.create(title: 'Family Guy', description: 'Funny show', created_at: 1.day.ago)
+      family_man = Video.create(title: 'Family Man', description: 'Funny show')
+      expect(Video.search_by_title("Family")).to eq([family_man, family_guy])
+    end 
+
+    it "returns an empty array a search with an empty string" do
+      family_guy = Video.create(title: 'Family Guy', description: 'Funny show')
+      south_park = Video.create(title: 'South Park', description: 'Funny show')
+      expect(Video.search_by_title("")).to eq([])
+    end
+
+    it "returns an array of one video if there is a similar match case insensitive" do
+      family_guy = Video.create(title: 'Family Guy', description: 'Funny show')
+      south_park = Video.create(title: 'South Park', description: 'Funny show')
+      expect(Video.search_by_title("fam")).to eq([family_guy])
+    end
+  end
+end
