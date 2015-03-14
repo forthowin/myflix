6d2b868a774f635d609712c7aa263f70a05bb7cb
Tho Nguyen <thogotmail@gmail.com>
2015-03-14 17:26:45 -0500

    Refactor sign in

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,18 +3,7 @@ require 'spec_helper'
 feature "user sign in" do
   scenario "with a valid account" do
     bob = Fabricate(:user, email: 'bob@myflix.com', password: 'password', full_name: 'Bob Bo')
-    visit sign_in_path
-    fill_in :email, with: 'bob@myflix.com'
-    fill_in :password, with: 'password'
-    click_button 'Sign in'
+    sign_in(bob)
     expect(page).to have_content(bob.full_name)
-  end
-
-  scenario "with an invalid account" do
-    visit sign_in_path
-    fill_in :email, with: 'jim@myflix.com'
-    fill_in :password, with: 'password'
-    click_button 'Sign in'
-    expect(page).to have_content('Invalid email or password.')
   end
 end
