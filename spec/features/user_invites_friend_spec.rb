7726504a8a6ebf6fd374bea2dbed521aa7fb4f98
Tho Nguyen <thogotmail@gmail.com>
2015-03-31 14:00:02 -0500

    add invite friend feature spec

---
+++ spec/features/user_invites_friend_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+feature "User invites friend" do
+  scenario "friend accepts invitation and the two follows each other" do
+    bob = Fabricate(:user)
+    sign_in(bob)
+
+    invite_a_friend
+    friend_accepts_invitation
+
+    friend_should_follow(bob)
+    inviter_should_follow_friend(bob)
+
+    clear_email
+  end
+
+  def invite_a_friend
+    visit invite_path
+    fill_in "Friend's Name", with: "Jim"
+    fill_in "Friend's Email Address", with: "jim@example.com"
+    fill_in "Invitation Message", with: "Hey man, join this awesome site!"
+    click_button "Send Invitation"
+    sign_out
+  end
+
+  def friend_accepts_invitation
+    open_email("jim@example.com")
+    current_email.click_link "Create an account at myflix"
+    fill_in "Password", with: "password"
+    fill_in "Full Name", with: "Jim Jo"
+    click_button "Sign Up"
+  end
+
+  def friend_should_follow(user)
+    click_link "People"
+    expect(page).to have_content user.full_name
+    sign_out
+  end
+
+  def inviter_should_follow_friend(user)
+    sign_in(user)
+    click_link "People"
+    expect(page).to have_content "Jim Jo"
+  end
+end
