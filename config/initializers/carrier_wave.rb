39107f22ee30da5f0de6e1e6703304a64af336dd
Tho Nguyen <thogotmail@gmail.com>
2015-04-11 00:10:54 -0500

    fix typo

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,6 +9,6 @@ CarrierWave.configure do |config|
     config.fog_directory  = ENV['BUCKET_DIRECTORY']                       # required
   else
     config.storage = :file
-    config.enable_processing = Rails.evn.development?
+    config.enable_processing = Rails.env.development?
   end
 end
